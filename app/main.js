e82bf7e7f4217bb0c48d5460a698223ee8fd0548
Bryan Eslinger <bryan.eslinger@protonmail.com>
2024-10-17 18:09:10 -0400

    Responds with hardcoded correlationId

--- app/main.js
+++ app/main.js
@@ -5,7 +5,13 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
-  // Handle connection
+  server.on("connection", (socket) => {
+    socket.on("data", () => {
+      const correlationId = 42
+      const buffer = Buffer.from[4, correlationId];
+      socket.end(buffer);
+    })
+  })
 });
 
 server.listen(9092, "127.0.0.1");
