a9f59668d251079b8f47423211b23539957bcc49
Bryan Eslinger <bryan.eslinger@protonmail.com>
2024-10-17 20:17:38 -0400

    Responds with hardcoded correlationId

--- app/main.js
+++ app/main.js
@@ -3,14 +3,31 @@ import net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+class Response {
+  constructor(correlationId) {
+    this.correlationId = correlationId;
+    this.messageLength = Buffer.alloc(4);
+  }
+
+  toBuffer() {
+    const message = Buffer.alloc(4);
+    message.writeInt32BE(this.correlationId);
+    this.messageLength.writeInt32BE(message.length);
+    return Buffer.concat([this.messageLength, message]);
+  }
+}
+
 // Uncomment this block to pass the first stage
-const server = net.createServer((connection) => {
-  server.on("connection", (socket) => {
-    socket.on("data", () => {
-      const correlationId = 42
-      const buffer = Buffer.from[4, correlationId];
-      socket.end(buffer);
-    })
+const server = net.createServer((socket) => {
+  socket.on("connection", (socket) => {
+    console.debug("new connection")
+  })
+
+  socket.on("data", () => {
+    console.debug("data received");
+    const response = new Response(7);
+    socket.write(response.toBuffer());
+    socket.end();
   })
 });
 
